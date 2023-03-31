17f21f2d879c1e378357a902bbb50bf7b956ff9a
Nathan Barros <nathanbarrosnt@gmail.com>
2023-03-31 08:43:50 -0300

    findById service improved

--- src/main/java/dev/natao/devnatao/services/ProductService.java
+++ src/main/java/dev/natao/devnatao/services/ProductService.java
@@ -2,7 +2,10 @@ package dev.natao.devnatao.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +16,11 @@ import dev.natao.devnatao.shared.ProductDTO;
 
 @Service
 public class ProductService {
-
+    
     @Autowired
     private ProductRepository productRepository;
 
-    public Product addProduct(Product product) {
+    public ProductDTO addProduct(ProductDTO product) {
         if (product.getProductQuantity() < 0)
             throw new NotFoundException("Product quantity cannot be zero.");
 
@@ -28,17 +31,28 @@ public class ProductService {
         productRepository.deleteById(id);
     }
 
-    public List<Product> findAll() {
-        return productRepository.findAll();
+    public List<ProductDTO> findAll() {
+        // Retornará uma List de Product (Model)
+        List<Product> products = productRepository.findAll();
+        // Convertendo em ProductDTO
+        return products.stream()
+            .map(product -> new ModelMapper().map(product, ProductDTO.class))
+            .collect(Collectors.toList());
     }
 
-    public Optional<Product> findById(Integer id) {
+    public Optional<ProductDTO> findById(Integer id) {
         if (id < 0) 
-            throw new NotFoundException("Product id must be greater than zero.");
-        return productRepository.findById(id);
+            throw new NotFoundException("Product id cannot be negative.");
+        // Obtem um Optional de product pelo ID
+        Optional<Product> product = productRepository.findById(id);
+        if (product.isEmpty()) throw new NotFoundException("Product not found");
+        // Converte o Optional<Product> product em um DTO
+        ProductDTO dto = new ModelMapper().map(product.get(), ProductDTO.class);
+        // Criando e retornando um Optional de ProductDTO.
+        return Optional.of(dto);
     }
 
-    public Product updateProduct(Integer id, Product product) {
+    public ProductDTO updateProduct(Integer id, ProductDTO product) {
         product.setProductId(id);
         return productRepository.save(product);
     }  
