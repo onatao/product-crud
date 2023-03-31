94b0ada64e699818bb92503146346de6b260ec6c
Nathan Barros <nathanbarrosnt@gmail.com>
2023-03-31 10:27:13 -0300

    removing unnecessary impots (ProductService)

--- src/main/java/dev/natao/devnatao/services/ProductService.java
+++ src/main/java/dev/natao/devnatao/services/ProductService.java
@@ -2,7 +2,6 @@ package dev.natao.devnatao.services;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
